552f85da3e60b718fd9a8bd2cabb56216b1ac513
andrei <luanvl@yandex.ru>
2025-04-22 18:05:48 +0300

    v2.2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "yc_token" {
   description = "Yandex Cloud OAuth token"
-  type        = t1.9euelZrKyMjOl4qYmcyViZPKlM-bne3rnpWaypmUnsaKypnGx4uLi52blpbl8_d9JGE_-e87Hzcv_N3z9z1TXj_57zsfNy_8zef1656VmpGdypeWmJHMypWMy8eOm8uV7_zF656VmpGdypeWmJHMypWMy8eOm8uV.0c9OZkmfw0egK0J3I1HVIRPbME3JjXN45X2kBHXDpfHWUal6ak5Uo-xVp7NVSoJIOe2KnJXbztQfpnSUj3nxCw
+  type        = string
 }
 
 variable "yc_cloud_id" {
   description = "Yandex Cloud ID"
-  type        = b1gms2nad5fs4qqeifo5
+  type        = string
 }
 
 variable "yc_folder_id" {
   description = "Yandex Folder ID"
-  type        = b1ge2dd9u82e5pqrc35i
+  type        = string
 }
