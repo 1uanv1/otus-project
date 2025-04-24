1487c302a080a2b2cd74044a7abdd24e6c6eee9d
andrei <luanvl@yandex.ru>
2025-04-24 19:45:10 +0300

    v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "key.json"
+  service_account_key_file = jsondecode(file("${path.module}/key.json"))
   zone      = "ru-central1-a"
 }
 
